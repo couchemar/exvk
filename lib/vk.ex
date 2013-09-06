0a31b08c60369e1af702bad6b348a77a733fda3e
Andrey Pavlov <couchemar@yandex.ru>
2013-09-06 18:14:35 +0400

    Отправка и отметка как прочитанное для сообщения

--- lib/vk.ex
+++ lib/vk.ex
@@ -11,8 +11,7 @@ defmodule Exvk.VK.Friends do
   end
 
   def api_get_online(query // []) do
-    method = "getOnline"
-    parsed = URI.parse(method)
+    parsed = URI.parse("getOnline")
     get(to_string(parsed.query(URI.encode_query(query))))
       .body["response"]
   end
@@ -31,8 +30,7 @@ defmodule Exvk.VK.Users do
   end
 
   def api_get(query // []) do
-    method = "get"
-    parsed = URI.parse(method)
+    parsed = URI.parse("get")
     body = get(to_string(parsed.query(URI.encode_query(query)))).body["response"]
     lc el inlist body do
       el |> ListDict.put("first_name",
@@ -62,16 +60,32 @@ defmodule Exvk.VK.Messages do
     |> Dict.merge(@defaults)
     |> Dict.put(:access_token, access_token)
     |> Dict.put(:user_id, user_id)
-    method = "getHistory"
-    parsed = URI.parse(method)
+    parsed = URI.parse("getHistory")
     body = get(to_string(parsed.query(URI.encode_query(query)))).body["response"]
-    items = body["items"]
-
-    items = lc item inlist items do
-      ListDict.put(item, "body",
-                   :unicode.characters_to_binary(item["body"], :utf8, :latin1))
-    end
-    ListDict.put(body, "items", items)
+    ListDict.put(
+      body, "items",
+      lc item inlist body["items"] do
+        ListDict.put(item, "body",
+                     :unicode.characters_to_binary(item["body"], :utf8, :latin1))
+      end)
   end
 
+  def api_send(access_token, user_id, message, query // []) do
+    query = query
+    |> Dict.merge(@defaults)
+    |> Dict.put(:access_token, access_token)
+    |> Dict.put(:user_id, user_id)
+    |> Dict.put(:message, message)
+    parsed = URI.parse("send")
+    get(to_string(parsed.query(URI.encode_query(query)))).body["response"]
+  end
+
+  def api_mark_as_read(access_token, sender_id, query // []) do
+    query = query
+    |> Dict.merge(@defaults)
+    |> Dict.put(:access_token, access_token)
+    |> Dict.put(:user_id, sender_id)
+    parsed = URI.parse("markAsRead")
+    get(to_string(parsed.query(URI.encode_query(query)))).body["response"]
+  end
 end
